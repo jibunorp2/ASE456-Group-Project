61fb6c078479c8a0c63020a0f398420a5bf8a301
sdy329 <sdy329@gmail.com>
2023-11-07 16:17:45 -0500

    Added Money Converter (Awaiting API_KEY)

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ase456_group_project/screens/simple.dart';
+import 'package:ase456_group_project/screens/money.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -24,20 +25,20 @@ class _HomePageState extends State<Home> {
       appBar: AppBar(
         title: const Text(''),
       ),
-      body: [Simple(), Item2()][tab],
+      body: [Simple(), Money()][tab],
       drawer: Drawer(
         child: ListView(
           children: [
             ListTile(
               leading: const Icon(Icons.arrow_right),
-              title: const Text("Item 1"),
+              title: const Text("Simple Calculator"),
               onTap: () {
                 selectItem(0);
               },
             ),
             ListTile(
               leading: const Icon(Icons.arrow_right),
-              title: const Text("Item 2"),
+              title: const Text("Money Converter"),
               onTap: () {
                 selectItem(1);
               },
@@ -46,51 +47,5 @@ class _HomePageState extends State<Home> {
         ),
       ),
     );
-  }
-}
-
-class Item1 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      alignment: Alignment.center,
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-                height: 80,
-                width: 260,
-                color: Colors.blueGrey,
-                alignment: Alignment.center,
-                child: const Text(
-                  "Item1",
-                  style: TextStyle(color: Colors.white, fontSize: 25),
-                )),
-          ]),
-    ));
-  }
-}
-
-class Item2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      alignment: Alignment.center,
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-                height: 80,
-                width: 260,
-                color: Colors.blueGrey,
-                alignment: Alignment.center,
-                child: const Text(
-                  "Item2",
-                  style: TextStyle(color: Colors.white, fontSize: 25),
-                )),
-          ]),
-    ));
   }
 }
---
+++ lib/screens/money.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Money extends StatefulWidget {
+  const Money({Key? key}) : super(key: key);
+
+  @override
+  State<Money> createState() => _MoneyState();
+}
+
+class _MoneyState extends State<Money> {
+  double _leftAmount = 0.0;
+  double _rightAmount = 0.0;
+  String _leftCurrency = 'USD';
+  String _rightCurrency = 'EUR';
+
+  final _leftController = TextEditingController();
+  final _rightController = TextEditingController();
+
+  List<String> currencyList = [
+    'USD',
+    'EUR',
+    'GBP',
+    'JPY',
+    'AUD',
+    'CAD',
+    'CHF',
+    'CNY',
+    'SEK',
+    'NZD',
+    'KRW',
+    'SGD',
+    'INR',
+    'BRL',
+    'ZAR',
+    'MXN',
+    'HKD',
+    'RUB',
+    'TRY',
+    'NOK',
+    'DKK',
+    'AED',
+    'SAR',
+    'THB',
+  ];
+
+  final apiKey = 'API_KEY';
+
+  Future<double> convertCurrencies(
+      String fromCurrency, String toCurrency, double amount) async {
+    final apiUrl =
+        Uri.https('api.apilayer.com', '/currency_conversion/v1/convert', {
+      'from': fromCurrency,
+      'to': toCurrency,
+      'amount': amount.toString(),
+    });
+
+    try {
+      final response = await http.get(
+        apiUrl,
+        headers: {
+          'api-key': apiKey,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final convertedAmount = data['result'];
+        return convertedAmount.toDouble();
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      return 0.0;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double boxWidth = MediaQuery.of(context).size.width * 0.4;
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: boxWidth,
+                  child: DropdownButton<String>(
+                    value: _leftCurrency,
+                    isExpanded: true,
+                    items: currencyList.map((String currency) {
+                      return DropdownMenuItem<String>(
+                        value: currency,
+                        child: Center(child: Text(currency)),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _leftCurrency = newValue!;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  width: boxWidth,
+                  child: TextField(
+                    controller: _leftController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Amount',
+                    ),
+                    onChanged: (String value) async {
+                      if (value.isEmpty) return;
+                      final convertedAmount = await convertCurrencies(
+                          _leftCurrency, _rightCurrency, _leftAmount);
+                      setState(() {
+                        _leftAmount = double.parse(value);
+                        _rightAmount = convertedAmount;
+                        _leftController.text = value;
+                        _rightController.text = convertedAmount.toString();
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: boxWidth,
+                  child: DropdownButton<String>(
+                    value: _rightCurrency,
+                    isExpanded: true,
+                    items: currencyList.map((String currency) {
+                      return DropdownMenuItem<String>(
+                        value: currency,
+                        child: Center(child: Text(currency)),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _rightCurrency = newValue!;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  width: boxWidth,
+                  child: TextField(
+                    controller: _rightController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Amount',
+                    ),
+                    onChanged: (String value) async {
+                      if (value.isEmpty) return;
+                      final convertedAmount = await convertCurrencies(
+                          _rightCurrency, _leftCurrency, _rightAmount);
+                      setState(() {
+                        _rightAmount = double.parse(value);
+                        _leftAmount = convertedAmount;
+                        _leftController.text = convertedAmount.toString();
+                        _rightController.text = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
